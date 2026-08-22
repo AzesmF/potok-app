ee703f9a2ea40e9c8d8f6de10beca18198be142d
AzesmF <azesmf@yandex.ru>
2026-08-22 10:40:05 +0300

    feat: добавить Flutter frontend с базовым экраном дневника и интеграцией API

---
+++ frontend/test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('PotokApp smoke test', (WidgetTester tester) async {
+    // TODO: Добавить интеграционные тесты после стабилизации UI
+    expect(true, isTrue);
+  });
+}
